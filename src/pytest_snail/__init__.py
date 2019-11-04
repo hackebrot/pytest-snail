1ea75497282b910339ddaa4fe66b4926c95f7e0d
Raphael Pierzina <raphael@hackebrot.de>
2019-11-04 12:36:11 +0100

    Add meta info to package init

---
+++ src/pytest_snail/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.0.rc1"
+__title__ = "pytest-snail"
+__description__ = "Plugin for adding a marker to slow running tests. 🐌"
+__url__ = "https://github.com/hackebrot/pytest-snail"
+
+__author__ = "Raphael Pierzina"
+__email__ = "raphael@hackebrot.de"
+
+__license__ = "MIT License"
+__copyright__ = "Copyright (c) 2019 Raphael Pierzina"
